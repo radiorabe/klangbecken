2dd5ecb9eedad2e42142717c072a24e442236fa2
Marco Schmalz <marco@schess.ch>
2017-03-21 02:12:34 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='KlangbeckenAPI',
+      version='0.0.1',
+      description='Klangbecken API',
+      author='Marco Schmalz',
+      author_email='marco@schess.ch ',
+      url='https://github.com/radiorabe/klangbecken',
+      py_modules=['klangbecken_api'],
+     )
