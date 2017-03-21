0b6794f4b2e1611545d83eab327c1b371c79b2ff
Marco Schmalz <marco@schess.ch>
2017-03-21 02:24:56 +0100

    Make tests a package

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,6 @@
 import unittest
 
+
 class BackendTest(unittest.TestCase):
     def setUp(self):
         pass
@@ -7,8 +8,8 @@ class BackendTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-
     def test_application(self):
         from klangbecken_api import application, KlangbeckenAPI
+
         self.assertTrue(isinstance(application, KlangbeckenAPI))
         self.assertTrue(callable(application))
