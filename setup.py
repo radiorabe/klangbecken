712f3106e9fbbf7293e3c10da6ed00b77eab6e42
Marco Schmalz <marco@schess.ch>
2017-03-22 13:54:33 +0100

    Small clean-ups

--- setup.py
+++ setup.py
@@ -2,20 +2,14 @@ from setuptools import setup
 
 setup(
     name='KlangbeckenAPI',
-    version='0.0.1',
+    version='0.0.2',
     description='Klangbecken API',
     url='https://github.com/radiorabe/klangbecken',
     author='Marco Schmalz',
     author_email='marco@schess.ch ',
     py_modules=['klangbecken_api'],
     license='AGPLv3',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
