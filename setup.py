fceba1a204de42cc8fa7e2653353045030231334
Marco Schmalz <marco@schess.ch>
2017-04-19 19:07:26 +0200

    liquidsoap: correct logging location

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='KlangbeckenAPI',
-    version='0.0.3',
+    version='0.0.4',
     description='Klangbecken API',
     url='https://github.com/radiorabe/klangbecken',
     author='Marco Schmalz',
