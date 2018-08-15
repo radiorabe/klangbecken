d9f4157554714ff87dfb9f44d1e0748b20b48a3a
Marco Schmalz <marco@schess.ch>
2018-08-15 20:24:46 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='KlangbeckenAPI',
-    version='0.0.6',
+    version='0.0.7',
     description='Klangbecken API',
     url='https://github.com/radiorabe/klangbecken',
     author='Marco Schmalz',
