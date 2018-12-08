77abd769175bcd300d2ba1c75a827a23f5cc0b2b
Marco Schmalz <marco@schess.ch>
2018-12-08 14:38:02 +0100

    Remove setting up WSGI application (do it in .wsgi file!)

--- klangbecken_api.py
+++ klangbecken_api.py
@@ -476,6 +476,3 @@ if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple('127.0.0.1', 5000, StandaloneWebApplication(),
                use_debugger=True, use_reloader=True, threaded=True)
-else:
-    # Set up WSGI application
-    application = KlangbeckenAPI()
