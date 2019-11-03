8d9e077af7be7ab483e1a8145218a4b634b893c1
Marco Schmalz <marco@schess.ch>
2019-11-03 15:38:46 +0100

    Typo

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
             'saemubox-listener=saemubox_listener:main',
             'klangbecken-import=klangecken_api:import_files',
             'klangbecken-fsck=klangbecken_api:fsck',
-            'klangbecken-log=logger:main',
+            'klangbecken-play-logger=logger:main',
         ],
     }
 )
