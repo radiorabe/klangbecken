cc87ce24ff9593dfce7847fb9c6f8cddae4e69dc
Marco Schmalz <marco@schess.ch>
2019-11-14 00:29:43 +0100

    Test with brand new Python 3.8

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
     ],
     install_requires=['docopt', 'mutagen', 'six', 'Werkzeug'],
