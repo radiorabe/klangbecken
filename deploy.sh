86f7f1d706b8b9b9cca3d17cf486b6ffe8e5850a
Marco Schmalz <marco@schess.ch>
2021-04-16 20:41:53 +0200

    chore: tag commit after successful deployment

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # abort on errors
 set -e
 
-SSH_HOST=root@vm-0016.vm-admin.int.rabe.ch
+SSH_HOST=$(git remote get-url prod | cut -d: -f1)
 
 if [ $# -eq 0 ]; then
   MOD_WSGI=yes
@@ -21,34 +21,77 @@ else
   exit 1
 fi
 
-if git diff --exit-code requirements.txt
+if ! git diff --exit-code requirements.txt
 then
-  TEMP=$(mktemp -d)
-  echo "############################"
-  echo "# Downloading dependencies #"
-  echo "############################"
-  pip download --dest "$TEMP" --no-binary :all: -r requirements.txt
-  if [ "$MOD_WSGI" = "yes" ]; then
+  echo "ERROR: Requirements file is dirty"
+  exit 1
+fi
+
+if ! git diff --exit-code setup.py
+then
+  echo "ERROR: `setup.py` is dirty"
+  exit 1
+fi
+
+if [ "$(git branch --show-current)" != "master" ]
+then
+    echo "ERROR: We are not on the 'master' branch."
+    exit 1
+fi
+
+INIT_VERSION=$(sed -n -e 's/^__version__\s*=\s*"\(.*\)"\s*/\1/p' klangbecken/__init__.py )
+SETUP_VERSION=$(sed -n -e 's/^\s*version\s*=\s*"\(.*\)".*$/\1/p' setup.py)
+
+if [ "$INIT_VERSION" != "$SETUP_VERSION" ]
+then
+    echo "ERROR: Version numbers in 'setup.py' and 'klangbecken/__init__.py' do not match."
+    exit 1
+fi
+VERSION="v$INIT_VERSION"
+
+TAG_VERSION=$(git tag -l "v*" | sort -V | tail -n 1)
+if [ "$VERSION" = "$TAG_VERSION" ]
+then
+    echo "ERROR: Version has not been incremented: $VERSION < $TAG_VERSION"
+    exit 1
+fi
+
+if [ "$((echo $VERSION; echo $TAG_VERSION) | sort -V | tail -n 1)" = "$TAG_VERSION" ]
+then
+    echo "ERROR: New version number is smaller than old one."
+    exit 1
+fi
+
+
+TEMP=$(mktemp -d)
+echo "############################"
+echo "# Downloading dependencies #"
+echo "############################"
+pip download --dest "$TEMP" --no-binary :all: -r requirements.txt
+if [ "$MOD_WSGI" = "yes" ]; then
     if ! pip download --dest "$TEMP" --no-binary :all: mod_wsgi; then
         echo
         echo "Note: Downloading mod_wsgi requires httpd-devel libraries"
         echo "      to be installed locally"
         exit 1
     fi
-  fi
-  echo
-  echo "##################################"
-  echo "# Copying dependencies to server #"
-  echo "##################################"
-  scp "$TEMP"/* "$SSH_HOST":"dependencies/"
-  rm "$TEMP"/*
-  rmdir "$TEMP"
-  echo
-  echo "#########################"
-  echo "# Deploying application #"
-  echo "#########################"
-  git push prod
-else
-  echo "WARNING: Requirements file is dirty"
-  exit 1
 fi
+echo
+echo "##################################"
+echo "# Copying dependencies to server #"
+echo "##################################"
+scp "$TEMP"/* "$SSH_HOST":"dependencies/"
+rm "$TEMP"/*
+rmdir "$TEMP"
+echo
+echo "#########################"
+echo "# Deploying application #"
+echo "#########################"
+git push prod master
+
+git tag $VERSION
+
+echo
+echo "Deployment successful!"
+echo "Push the newly created version tag to the upstream repository with"
+echo "    `git push --tags upstream master`"
