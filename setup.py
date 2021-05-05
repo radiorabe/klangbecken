9472a253f1a14806edc59142326226c8409d7b88
spameier <spameier@users.noreply.github.com>
2021-05-05 21:26:53 +0200

    bump to 0.0.17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="klangbecken",
-    version="0.0.16",
+    version="0.0.17",
     description="Klangbecken Audio Player",
     url="https://github.com/radiorabe/klangbecken",
     author="Marco Schmalz",
