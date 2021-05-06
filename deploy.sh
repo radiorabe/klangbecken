6d64abb05fcee3fe12f67039193dd50931861dd3
Marco Schmalz <marco@schess.ch>
2021-05-06 15:16:20 +0200

    fix(deploy.sh): increment version number and more checks

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # abort on errors
 set -e
 
-SSH_HOST=$(git remote get-url prod | cut -d: -f1)
-
+# "parse" command line arguments
 if [ $# -eq 0 ]; then
   MOD_WSGI=yes
 elif [ $# -eq 1 ] && [ "$1" = "--no-mod-wsgi" -o "$1" = "-n" ]; then
@@ -21,25 +20,66 @@ else
   exit 1
 fi
 
-if ! git diff --exit-code requirements.txt
-then
-  echo "ERROR: Requirements file is dirty"
-  exit 1
-fi
+echo "##################"
+echo "# Perform checks #"
+echo "##################"
 
-if ! git diff --exit-code setup.py
-then
-  echo "ERROR: `setup.py` is dirty"
-  exit 1
+# are there prod and upstream remotes?
+if ! git remote | grep --quiet "^prod$"; then
+    echo "ERROR: No 'prod' remote configured"
+    exit 1
 fi
-
-if [ "$(git branch --show-current)" != "master" ]
-then
-    echo "ERROR: We are not on the 'master' branch."
+if ! git remote | grep --quiet "^upstream$"; then
+    echo "ERROR: No 'upstream' remote configured"
     exit 1
 fi
 
-INIT_VERSION=$(sed -n -e 's/^__version__\s*=\s*"\(.*\)"\s*/\1/p' klangbecken/__init__.py )
+PROD_HOST=$(git remote get-url prod | cut -d: -f1)
+
+# are we clean?
+if ! git diff --exit-code --quiet; then
+    echo "ERROR: Working directory is NOT clean."
+    exit 1
+fi
+
+# are we on the master branch?
+if [ "$(git branch --show-current)" != "master" ]
+then
+    echo "ERROR: We are NOT on the 'master' branch."
+    exit 1
+fi
+
+# fetch latest version from upstream
+if ! git fetch upstream master; then
+    echo
+    echo "ERROR: cannot fetch current upstream version."
+    exit 1
+fi
+
+# are we pointing at upstream/master?
+if ! git show --no-patch --pretty=format:%D | grep --quiet upstream/master
+then
+    echo "ERROR: 'master' branch is NOT pointing at 'upstream/master'."
+    echo "       Make sure, your local version is in sync with upstream."
+    exit 1
+fi
+
+# check connection to prod and fetch state
+if ! git fetch prod master; then
+    echo "ERROR: cannot connect to prod to fetch current version."
+    exit 1
+fi
+
+# is prod/master already up to date?
+if git show --no-patch --pretty=format:%D | grep --quiet prod/master
+then
+    echo
+    echo "ERROR: 'prod' is already up to date."
+    exit 1
+fi
+
+# check current version
+INIT_VERSION=$(sed -n -e 's/^__version__\s*=\s*"\(.*\)"\s*$/\1/p' klangbecken/__init__.py )
 SETUP_VERSION=$(sed -n -e 's/^\s*version\s*=\s*"\(.*\)".*$/\1/p' setup.py)
 
 if [ "$INIT_VERSION" != "$SETUP_VERSION" ]
@@ -47,26 +87,43 @@ then
     echo "ERROR: Version numbers in 'setup.py' and 'klangbecken/__init__.py' do not match."
     exit 1
 fi
-VERSION="v$INIT_VERSION"
 
-TAG_VERSION=$(git tag -l "v*" | sort -V | tail -n 1)
-if [ "$VERSION" = "$TAG_VERSION" ]
+TAG_VERSION=$(git tag --merged HEAD -l "v*" | sort -V | tail -n 1)
+if [ "v$SETUP_VERSION" != "$TAG_VERSION" ]
 then
-    echo "ERROR: Version has not been incremented."
+    echo "ERROR: Tag and package versions do not match."
     exit 1
 fi
 
-if [ "$((echo $VERSION; echo $TAG_VERSION) | sort -V | tail -n 1)" = "$TAG_VERSION" ]
-then
-    echo "ERROR: New version number is smaller than old one: $VERSION < $TAG_VERSION"
+echo
+echo -n "Everything looks good. Start deployment? [Y/n]"
+read -n 1 ANS
+
+if ! [ -z "$ANS" -o "$ANS" = "y" -o "$ANS" = "Y" ]; then
+    echo "    Bye ..."
     exit 1
 fi
 
+echo
+echo "#####################"
+echo "# Increment version #"
+echo "#####################"
+OLD_VERSION=$SETUP_VERSION
+LAST_DIGIT=$(echo "$OLD_VERSION" | cut -d. -f3)
+LAST_DIGIT=$((LAST_DIGIT + 1))
+NEW_VERSION=$(echo "$OLD_VERSION" | cut -d. -f1-2)."$LAST_DIGIT"
+
+sed -i "s/__version__ = \"$OLD_VERSION\"/__version__ = \"$NEW_VERSION\"/" klangbecken/__init__.py
+sed -i "s/version=\"$OLD_VERSION\",/version=\"$NEW_VERSION\",/" setup.py
+
+git add klangbecken/__init__.py setup.py
+git commit -m "Version bump v$NEW_VERSION"
 
 TEMP=$(mktemp -d)
-echo "############################"
-echo "# Downloading dependencies #"
-echo "############################"
+echo
+echo "#########################"
+echo "# Download dependencies #"
+echo "#########################"
 pip download --dest "$TEMP" --no-binary :all: -r requirements.txt
 if [ "$MOD_WSGI" = "yes" ]; then
     if ! pip download --dest "$TEMP" --no-binary :all: mod_wsgi; then
@@ -76,22 +133,27 @@ if [ "$MOD_WSGI" = "yes" ]; then
         exit 1
     fi
 fi
+
 echo
-echo "##################################"
-echo "# Copying dependencies to server #"
-echo "##################################"
-scp "$TEMP"/* "$SSH_HOST":"dependencies/"
+echo "###############################"
+echo "# Copy dependencies to server #"
+echo "###############################"
+scp "$TEMP"/* "$PROD_HOST":"dependencies/"
 rm "$TEMP"/*
 rmdir "$TEMP"
+
 echo
-echo "#########################"
-echo "# Deploying application #"
-echo "#########################"
+echo "######################"
+echo "# Deploy application #"
+echo "######################"
 git push prod master
 
-git tag $VERSION
+echo
+echo "#######################"
+echo "# Finalize deployment #"
+echo "#######################"
+git tag "v$NEW_VERSION"
+git push upstream --tags
 
 echo
 echo "Deployment successful!"
-echo "Push the newly created version tag to the upstream repository with"
-echo "    git push --tags upstream master"
