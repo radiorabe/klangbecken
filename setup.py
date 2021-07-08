dead63542857bc689f007a933a7be4cc378d5619
Marco Schmalz <marco@schess.ch>
2021-07-09 00:52:07 +0200

    Version bump v0.0.27

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="klangbecken",
-    version="0.0.26",
+    version="0.0.27",
     description="Klangbecken Audio Player",
     url="https://github.com/radiorabe/klangbecken",
     author="Marco Schmalz",
