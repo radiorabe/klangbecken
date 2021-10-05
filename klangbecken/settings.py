9dc0d9bfd05582009b9a0ecef4f751fd8186a615
Marco Schmalz <marco@schess.ch>
2021-10-05 17:44:02 +0200

    doc(settings): code documentation

--- klangbecken/settings.py
+++ klangbecken/settings.py
@@ -1,12 +1,17 @@
 import mutagen.mp3
 
+# Supported playlist names
 PLAYLISTS = ("music", "classics", "jingles")
 
+# Supported file types
 # Map file extension to mutagen class for all supported file types
 FILE_TYPES = {
     "mp3": mutagen.mp3.EasyMP3,
 }
 
+# Supported datetime format
+# ISO8601 datetime with optional fraction of a second (milli- or microseconds) and
+# mandatory timezone specification
 ISO8601_TZ_AWARE_RE = (
     # Date
     r"(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T"
@@ -16,6 +21,17 @@ ISO8601_TZ_AWARE_RE = (
     r"[+-](?:2[0-3]|[01][0-9]):[0-5][0-9]"
 )
 
+# Supported Metadata
+# Keys map to type (and contract) checks.
+# A check can be a:
+# - Type class (e.g. float)
+# - Function taking one argument and returning True or False
+#   (e.g. lambda x: x > 0 for positive numbers)
+# - String interpreted as a regular expression. The checked values are
+#   expected to be strings. (e.g. r"[1-9][0-9]{3}" for four digit zip codes)
+# - List or tuple cotaining an arbitrary combination of the above.
+#
+# Note: The checks are evaluated in their specified order.
 METADATA = {
     "id": r"^[a-z0-9]{8}-([a-z0-9]{4}-){3}[a-z0-9]{12}$",
     "ext": (str, lambda ext: ext in FILE_TYPES.keys()),
@@ -37,12 +53,16 @@ METADATA = {
     "expiration": r"(^$)|(^{0}$)".format(ISO8601_TZ_AWARE_RE),
 }
 
+# Metadata keys allowed for updates
 UPDATE_KEYS = "artist title weight expiration".split()
 
+# Metadata keys stored in the audio track (as ID3 tags)
 TAG_KEYS = "artist title cue_in cue_out track_gain original_filename last_play".split()
 
+# Metadata keys used for logging
 LOG_KEYS = "id playlist original_filename artist title play_count last_play".split()
 
+# Register additional ID3 tags
 mutagen.easyid3.EasyID3.RegisterTXXXKey(key="cue_in", desc="CUE_IN")
 mutagen.easyid3.EasyID3.RegisterTXXXKey(key="cue_out", desc="CUE_OUT")
 mutagen.easyid3.EasyID3.RegisterTXXXKey(key="track_gain", desc="REPLAYGAIN_TRACK_GAIN")
