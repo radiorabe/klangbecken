398f026482d21621a4c87ab5d86ca4b2da25cca3
Marco Schmalz <marco@schess.ch>
2021-10-08 18:07:30 +0200

    Version bump v0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="klangbecken",
-    version="0.0.36",
+    version="0.1.0",
     description="Klangbecken Audio Player",
     long_description=long_description,
     long_description_content_type="text/markdown",
